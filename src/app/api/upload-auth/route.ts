617b47a80a12cf953a5a6f5dc21d54b90f2435fd
csfahad <dev.csfahad@gmail.com>
2026-01-17 23:32:17 +0530

    feat(imagekit-auth): implement imagekit-auth api

---
+++ src/app/api/upload-auth/route.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+import { getUploadAuthParams } from "@imagekit/next/server";
+
+export async function GET() {
+  try {
+    // generate upload authentication parameters
+    const { token, expire, signature } = getUploadAuthParams({
+      privateKey: process.env.IMAGEKIT_PRIVATE_KEY as string,
+      publicKey: process.env.IMAGEKIT_PUBLIC_KEY as string,
+    });
+
+    return NextResponse.json({
+      token,
+      expire,
+      signature,
+      publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+    });
+  } catch (err) {
+    console.error("Auth error:", err);
+    return NextResponse.json(
+      { error: "Failed to generate auth params" },
+      { status: 500 }
+    );
+  }
+}
