960b1ddfb3db05adbe920ba98a43cabb43692225
csfahad <dev.csfahad@gmail.com>
2026-02-06 20:03:55 +0530

    fix(pricing): fix font-typography

--- src/modules/pricing/index.tsx
+++ src/modules/pricing/index.tsx
@@ -265,7 +265,7 @@ export default function Pricing() {
                                             <div className="shrink-0 w-5 h-5 rounded-full bg-primary/20 flex items-center justify-center">
                                                 <Check className="w-3 h-3 text-primary" />
                                             </div>
-                                            <span className="text-foreground">
+                                            <span className="text-muted-foreground">
                                                 {feature}
                                             </span>
                                         </div>
